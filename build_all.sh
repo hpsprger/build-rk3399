902f561a6b97ad56a7a555b87f6451747c211b9a
hpsprger <281872614@qq.com>
2022-10-27 13:26:51 +0800

    add new script:build_all.sh

---
+++ build_all.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+
+LOCALPATH=$(pwd)
+
+TOPDIR=${LOCALPATH}/..
+
+finish() {
+	echo -e "\e[31m build all failed.\e[0m"
+	exit -1
+}
+trap finish ERR
+
+cd ${TOPDIR}
+
+./build/mk-uboot.sh rockpi4b
+echo -e "\e[32m build uboot done ...\e[0m"
+
+./build/mk-kernel.sh rockpi4b
+echo -e "\e[32m build kernel done ...\e[0m"
+
+
+
+if [ ! -e ${TOPDIR}/rootfs/ubuntu-build-service/buster-desktop-arm64/linaro-$RELEASE-alip-*.tar.gz ]; then
+	cd ${TOPDIR}/rootfs
+	export ARCH=arm64
+	sudo apt-get install binfmt-support qemu-user-static gdisk
+	sudo dpkg -i ubuntu-build-service/packages/*        # ignore the broken dependencies, we will fix it next step
+	sudo apt-get install -f
+	RELEASE=buster TARGET=desktop ARCH=${ARCH} ./mk-base-debian.sh
+	VERSION=debug ARCH=${ARCH} ./mk-rootfs-buster.sh  && ./mk-image.sh
+	echo -e "\e[32m  rootfs done  ...\e[0m"
+else
+	echo -e "\e[32m  rootfs already ok  ...\e[0m"
+fi
+
+cd ${TOPDIR}
+# Generate system image with two partitions
+build/mk-image.sh -c rk3399 -t system -r rootfs/linaro-rootfs.img
+
+# Generate ROCK Pi 4 system image with five partitions.
+# build/mk-image.sh -c rk3399 -t system -r rootfs/linaro-rootfs.img
+
+echo -e "\e[36m all READY! \e[0m"
