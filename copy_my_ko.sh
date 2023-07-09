bd29c4204da31011699a76cdd96b33b6cae5ce82
hpsprger <281872614@qq.com>
2023-07-09 21:55:42 +0800

    mdf

--- copy_my_ko.sh
+++ copy_my_ko.sh
@@ -3,6 +3,6 @@
 echo "copy my ko start..."
 echo "copy rockpi4b_ko_for_testing_kernel.ko"
 cp  -f /home/hpsp/rock_space/my_code_wheels/my_code_wheels/fast_ko/kernel_code/rockpi4b_ko/rockpi4b_ko_for_testing_kernel.ko   /home/hpsp/rock_space/rockpi_4b/rockchip-bsp/rootfs/binary/home/linaro 
-cp  -f /home/hpsp/rock_space/ethtool/ethtool/ethtool    
-ls  -alh /home/hpsp/rock_space/rockpi_4b/rockchip-bsp/rootfs/binary/home/linaro                                                                       /home/hpsp/rock_space/rockpi_4b/rockchip-bsp/rootfs/binary/home/linaro 
+cp  -f /home/hpsp/rock_space/ethtool/ethtool/ethtool    /home/hpsp/rock_space/rockpi_4b/rockchip-bsp/rootfs/binary/home/linaro 
+ls  -alh /home/hpsp/rock_space/rockpi_4b/rockchip-bsp/rootfs/binary/home/linaro  
 echo "copy my ko end..."
